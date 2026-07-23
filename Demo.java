92e2ce2e5f6f9933a264715a0f09ae191b20b5fa
shenalmihisara <shenalmihisara51@gmail.com>
2026-07-23 18:36:11 +0530

    Refactor Demo class to dynamically create and add buttons to JFrame

--- Demo.java
+++ Demo.java
@@ -6,14 +6,28 @@ class Demo{
 		f1.setSize(300,300);
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-		JButton b1 = new JButton();
-		JButton b2 = new JButton();
-		JButton b3 = new JButton();
-		JButton b4 = new JButton();
-		JButton b5 = new JButton();
 		
-		b1.setText("North");
+		JButton[] btArray = new JButton[5];
+		String[] buttonText = {"North","East","South","West","Center"};
+		
+		for(int i=0; i<5; i++){
+			btArray[i] = new JButton(buttonText[i]);
+			btArray[i].setSize(300,400);
+			btArray[i].setFont(new Font("",2,30));
+
+			f1.add(buttonText[i],btArray[i]);
+			
+		}
+		f1.pack();
+		f1.setVisible(true);
+
+		//JButton b1 = new JButton();
+		//JButton b2 = new JButton();
+		//JButton b3 = new JButton();
+		//JButton b4 = new JButton();
+		//JButton b5 = new JButton();
+		
+		/*b1.setText("North");
 		b1.setFont(new Font("",1,30));
 		f1.add("North",b1);
 
@@ -31,9 +45,6 @@ class Demo{
 
 		b5.setText("Center");
 		b5.setFont(new Font("",1,30));
-		f1.add("Center",b5);
-
-		f1.pack();
-		f1.setVisible(true);
+		f1.add("Center",b5);*/
 	}
 }
