544dd6ac8131bdb2fc48e2584a9f0ebd56a0d0a0
shenalmihisara <shenalmihisara51@gmail.com>
2026-07-23 23:15:09 +0530

    Refactor Demo class to implement BorderLayout and FlowLayout with JTextField and JButton

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
+/*import java.awt.*;
 import javax.swing.*;
-import java.awt.*;
 class Demo{
 	public static void main(String args[]){
 		JFrame f1=new JFrame("Calculator");	 //String->Title
@@ -29,18 +29,53 @@ class Demo{
 		f1.setVisible(true);
 		
 		
-		/*JButton[] btArray = new JButton[5];
-		String[] buttonText = {"North","East","South","West","Center"};
+		//JButton[] btArray = new JButton[5];
+		//String[] buttonText = {"North","East","South","West","Center"};
 		
-		for(int i=0; i<5; i++){
-			btArray[i] = new JButton(buttonText[i]);
-			btArray[i].setSize(300,400);
-			btArray[i].setFont(new Font("",2,30));
+		//for(int i=0; i<5; i++){
+		//	btArray[i] = new JButton(buttonText[i]);
+		//	btArray[i].setSize(300,400);
+		//	btArray[i].setFont(new Font("",2,30));
 
-			f1.add(buttonText[i],btArray[i]);
+		//	f1.add(buttonText[i],btArray[i]);
 			
-		}
-		f1.pack();
-		f1.setVisible(true);*/
+		//}
+		//f1.pack();
+		//f1.setVisible(true);
+	}
+}*/
+
+
+import javax.swing.*;
+import java.awt.*;
+
+class Demo{
+	public static void main(String args[]){
+		JFrame f1=new JFrame("BorderLayout");	
+		JFrame f2=new JFrame("FlowLayout");	
+		f1.setSize(300,300);
+		f2.setSize(300,300);
+		f1.setLocationRelativeTo(null);
+		f2.setLocationRelativeTo(null);
+		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		f2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		f1.setLayout(new BorderLayout());//->Default BorderLayout
+		f2.setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		JTextField txtBorderLayout=new JTextField(5);
+		JTextField txtFlowLayout=new JTextField(5);
+		
+		txtBorderLayout.setFont(new Font("",1,30));
+		txtFlowLayout.setFont(new Font("",1,30));
+		
+		f1.add("North",txtBorderLayout);
+		f1.add("South",new JButton("Test"));
+		
+		f2.add(txtFlowLayout);
+		
+		f1.setVisible(true);
+		f2.setVisible(true);
 	}
 }
+
