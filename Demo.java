b86e1310bf0d610d290d35b1d3cb740ab0b4dbd1
shenalmihisara <shenalmihisara51@gmail.com>
2026-07-25 15:50:42 +0530

    Refactor Demo class to restore and enhance JFrame setup with GridLayout and button initialization

--- Demo.java
+++ Demo.java
@@ -46,8 +46,8 @@ class Demo{
 }*/
 
 
-import javax.swing.*;
-import java.awt.*;
+//import javax.swing.*;
+//import java.awt.*;
 
 /*class Demo{
 	public static void main(String args[]){
@@ -79,7 +79,7 @@ import java.awt.*;
 	}
 }*/
 
-import javax.swing.*;
+/*import javax.swing.*;
 import java.awt.*;
 
 class Demo{
@@ -94,6 +94,29 @@ class Demo{
 		String[] buttonText = {"Add","Delete","Update","Read","Cancle","Exit"};
 		
 		for(int i=0; i<6; i++){
+			btArray[i] = new JButton(buttonText[i]);
+			btArray[i].setFont(new Font("",1,30));
+			f1.add(buttonText[i],btArray[i]);
+		}
+		f1.setVisible(true);
+	}
+}*/
+
+import java.awt.*;
+import javax.swing.*;
+
+class Demo{
+	public static void main(String[] args){
+		JFrame f1 = new JFrame("Calculator");
+		f1.setSize(300,300);
+		f1.setLocationRelativeTo(null);
+		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		f1.setLayout(new GridLayout(4,4));
+		JButton[] btArray = new JButton[16];
+		String[] buttonText = {"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-",};
+		
+		for(int i=0; i<16; i++){
 			btArray[i] = new JButton(buttonText[i]);
 			btArray[i].setFont(new Font("",1,30));
 			f1.add(buttonText[i],btArray[i]);
