eb533333004105bfd16181dd93b471eb42e125aa
shenalmihisara <shenalmihisara51@gmail.com>
2026-07-25 23:23:55 +0530

    Refactor Demo class to enhance JFrame setup with JTextField and JPanel for button layout

--- Demo.java
+++ Demo.java
@@ -102,31 +102,7 @@ class Demo{
 	}
 }*/
 
-
-//import java.awt.*;
-//import javax.swing.*;
-
-/*class Demo{
-	public static void main(String[] args){
-		JFrame f1 = new JFrame("Calculator");
-		f1.setSize(300,300);
-		f1.setLocationRelativeTo(null);
-		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
-		f1.setLayout(new GridLayout(4,4));
-		JButton[] btArray = new JButton[16];
-		String[] buttonText = {"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-",};
-		
-		for(int i=0; i<16; i++){
-			btArray[i] = new JButton(buttonText[i]);
-			btArray[i].setFont(new Font("",1,30));
-			f1.add(buttonText[i],btArray[i]);
-		}
-		f1.setVisible(true);
-	}
-}*/
-
-import java.awt.*;
+/*import java.awt.*;
 import javax.swing.*;
 class Demo{
 	public static void main(String[] args){
@@ -173,5 +149,36 @@ class Demo{
 		f1.pack();
 		f1.setVisible(true);
 	}
-}
+}*/
 
+import java.awt.*;
+import javax.swing.*;
+
+class Demo{
+	public static void main(String[] args){
+				
+		JFrame f1 = new JFrame("Calculator");
+		f1.setSize(300,300);
+		f1.setLocationRelativeTo(null);
+		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		JTextField TxtDisplay = new JTextField();
+		TxtDisplay.setFont(new Font("",1,30));
+		f1.add("North",TxtDisplay);
+		
+		JPanel Btn = new JPanel();
+		Btn.setLayout(new GridLayout(4,4));
+		
+		JButton[] btArray = new JButton[16];
+		String[] buttonText = {"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-",};
+		
+		for(int i=0; i<16; i++){
+			btArray[i] = new JButton(buttonText[i]);
+			btArray[i].setFont(new Font("",1,30));
+			Btn.add(buttonText[i],btArray[i]);
+		}
+		f1.add("Center",Btn);
+		
+		f1.setVisible(true);
+	}
+}
