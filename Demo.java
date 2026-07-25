95df6056814de30f6f2fc7dce2a7ae9d096c83f8
shenalmihisara <shenalmihisara51@gmail.com>
2026-07-25 15:29:00 +0530

    Refactor Demo class to enhance JFrame setup and button initialization with GridLayout

--- Demo.java
+++ Demo.java
@@ -49,7 +49,7 @@ class Demo{
 import javax.swing.*;
 import java.awt.*;
 
-class Demo{
+/*class Demo{
 	public static void main(String args[]){
 		JFrame f1=new JFrame("BorderLayout");	
 		JFrame f2=new JFrame("FlowLayout");	
@@ -76,6 +76,29 @@ class Demo{
 		
 		f1.setVisible(true);
 		f2.setVisible(true);
+	}
+}*/
+
+import javax.swing.*;
+import java.awt.*;
+
+class Demo{
+	public static void main(String[] args){
+		JFrame f1 = new JFrame("BorderLayout");
+		f1.setSize(300,300);
+		f1.setLocationRelativeTo(null);
+		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		f1.setLayout(new GridLayout(3,2));
+		JButton[] btArray = new JButton[6];
+		String[] buttonText = {"Add","Delete","Update","Read","Cancle","Exit"};
+		
+		for(int i=0; i<6; i++){
+			btArray[i] = new JButton(buttonText[i]);
+			btArray[i].setFont(new Font("",1,30));
+			f1.add(buttonText[i],btArray[i]);
+		}
+		f1.setVisible(true);
 	}
 }
 
