d19edd7044b3b269915f0091638febad4e11fdf2
shenalmihisara <shenalmihisara51@gmail.com>
2026-08-04 09:32:59 +0530

    Add AddStudentForm class with GUI components for student data entry

--- Demo.java
+++ Demo.java
@@ -340,7 +340,7 @@ class Demo{
 	}
 }*/
 
-import java.awt.*;
+/*import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -370,4 +370,109 @@ class Demo{
 		Calculator c1=new Calculator("Calculator");
 		c1.setVisible(true);
 	}
+}*/
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class AddStudentForm  extends JFrame{
+	private JTextField txtStudentId;
+	private JTextField txtName;
+	private JTextField txtPrfMarks;
+	private JTextField txtDbmsMarks;
+	
+	private JButton btCancel;
+	private JButton btAdd;
+	
+	AddStudentForm(){
+		setSize(400,300);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		JLabel titleLabel=new JLabel("Add Student Form");
+		titleLabel.setFont(new Font("",1,27));
+		titleLabel.setHorizontalAlignment(JLabel.CENTER);
+		add("North",titleLabel);
+		//-------------------------------------------------------------
+		
+		JPanel southPanel=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		btAdd=new JButton("Add Student");
+		
+		btAdd.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt){
+				System.out.println("You pressed add...");
+				System.exit(0);
+			}
+		});
+		
+		btCancel=new JButton("Cancel");
+		
+		btCancel.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt){
+				System.out.println("You pressed cancle...");
+				System.exit(0);
+			}
+		});
+		
+		btAdd.setFont(new Font("",1,20));
+		btCancel.setFont(new Font("",1,20));
+		
+		southPanel.add(btAdd);
+		southPanel.add(btCancel);
+		add("South",southPanel);
+		//-------------------------------------------------------------
+		
+		JLabel lblStudentId=new JLabel("Student ID");
+		JLabel lblName=new JLabel("Name");
+		JLabel lblPrfMarks=new JLabel("Prf Marks");
+		JLabel lblDbmsMarks=new JLabel("Dbms Marks");
+		lblStudentId.setFont(new Font("",1,20));
+		lblName.setFont(new Font("",1,20));
+		lblPrfMarks.setFont(new Font("",1,20));
+		lblDbmsMarks.setFont(new Font("",1,20));
+		
+		JPanel labelPanel=new JPanel(new GridLayout(4,1));
+		JPanel idTextPanal=new JPanel(new FlowLayout(FlowLayout.LEFT));
+		labelPanel.add(lblStudentId);
+		labelPanel.add(lblName);
+		labelPanel.add(lblPrfMarks);
+		labelPanel.add(lblDbmsMarks);
+		add("West",labelPanel);
+		
+		//-------------------------------------------------------------
+		txtStudentId=new JTextField(5);
+		txtStudentId.setFont(new Font("",1,20));
+		txtName=new JTextField(10);
+		txtName.setFont(new Font("",1,20));
+		txtPrfMarks=new JTextField(4);
+		txtPrfMarks.setFont(new Font("",1,20));
+		txtDbmsMarks=new JTextField(4);
+		txtDbmsMarks.setFont(new Font("",1,20));
+		
+		JPanel textPanel=new JPanel(new GridLayout(4,1));
+		
+		JPanel idTextPanel=new JPanel(new FlowLayout(FlowLayout.LEFT));
+		idTextPanal.add(txtStudentId);
+		textPanel.add(idTextPanal);
+		
+		JPanel nameTextPanal=new JPanel(new FlowLayout(FlowLayout.LEFT));
+		nameTextPanal.add(txtName);
+		textPanel.add(nameTextPanal);
+		
+		JPanel prfMarksTextPanal=new JPanel(new FlowLayout(FlowLayout.LEFT));
+		prfMarksTextPanal.add(txtPrfMarks);
+		textPanel.add(prfMarksTextPanal);
+		
+		JPanel dbmsMarksTextPanal=new JPanel(new FlowLayout(FlowLayout.LEFT));
+		dbmsMarksTextPanal.add(txtDbmsMarks);
+		textPanel.add(dbmsMarksTextPanal);
+		add("Center",textPanel);
+	}
 }
+class Demo{
+	public static void main(String args[]){
+		new AddStudentForm().setVisible(true);
+	}
+}
+
